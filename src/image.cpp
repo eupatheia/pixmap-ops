93f87dbbe9411b958a5ad4d18a133129485b090f
eupatheia <75283980+eupatheia@users.noreply.github.com>
2023-02-02 14:25:32 -0500

    pre-debugging code

--- src/image.cpp
+++ src/image.cpp
@@ -1,4 +1,11 @@
-// Copyright 2021, Aline Normoyle, alinen
+// Base code Copyright 2021, Aline Normoyle, alinen
+
+/* image.cpp
+ *
+ *
+ * STB documentation can be found at
+ * https://github.com/nothings/stb/
+ */
 
 #include "image.h"
 
@@ -11,182 +18,281 @@
 namespace agl {
 
 
-Image::Image() {
-}
+Image::Image() {  }
 
-Image::Image(int width, int height)  {
-
+Image::Image(int width, int height): _width(width), _height(height) {
+  _pixels = (struct Pixel *) malloc(sizeof(struct Pixel *) * width * height);
+  _need_to_free_malloc = true;
 }
 
 Image::Image(const Image& orig) {
-  
+  _width = orig._width;
+  _height = orig._height;
+  _components = orig._components;
+  _pixels = orig._pixels;
 }
 
 Image& Image::operator=(const Image& orig) {
   if (&orig == this) {
     return *this;
   }
-
+  _width = orig._width;
+  _height = orig._height;
+  _components = orig._components;
+  _pixels = orig._pixels;
   return *this;
 }
 
 Image::~Image() {
+  if (_need_to_free_stbi) {
+    // was allocated by stbi_load, need to stbi free
+    stbi_image_free(_pixels);
+  } else if (_need_to_free_malloc) {
+    // was malloc-ed, need to free
+    free(_pixels);
+  }
 }
 
-int Image::width() const { 
-   return 0;
+int Image::width() const {
+  return _width;
 }
 
 int Image::height() const {
-   return 0;
+  return _height;
 }
 
 char* Image::data() const {
-   return 0;
+  return (char *) _pixels;
 }
 
 void Image::set(int width, int height, unsigned char* data) {
+  _width = width;
+  _height = height;
+  _pixels = (struct Pixel *) data;
 }
 
 bool Image::load(const std::string& filename, bool flip) {
-  return false;
+  // if flip = true, will set stbi's flip variable to true also
+  // auto conversion to bool (false = 0, true = 1)
+  stbi_set_flip_vertically_on_load(flip);
+  // stbi_load returns unsigned char *, so must cast to struct Pixel *
+  // also must convert filename from string to char *
+  _pixels = (struct Pixel *) stbi_load(filename.c_str(), &_width, &_height,
+      &_components, 3);
+  _need_to_free_stbi = true;
+  // reset to false for future calls
+  stbi_set_flip_vertically_on_load(false);
+  if (_pixels == NULL) {
+    // allocation failure
+    return false;
+  } else {
+    // successful load
+    return true;
+  }
 }
 
-
 bool Image::save(const std::string& filename, bool flip) const {
-  return false;
+  // if flip = true, will set stbi's flip variable to true also
+  stbi_flip_vertically_on_write(flip);
+  int result = stbi_write_png(filename.c_str(), _width, _height, 3,
+      (void *) _pixels, sizeof(struct Pixel) * _width);
+  // reset to false for future calls
+  stbi_flip_vertically_on_write(flip);
+  if (result == 0) {
+    // failed to write file
+    return false;
+  } else {
+    // successful write
+    return true;
+  }
 }
 
 Pixel Image::get(int row, int col) const {
-  return Pixel{ 0, 0, 0 };
+  return _pixels[row * _width + col];
 }
 
 void Image::set(int row, int col, const Pixel& color) {
- 
+  _pixels[row * _width + col] = color;
 }
 
-
-Pixel Image::get(int i) const
-{
-   return Pixel{ 0, 0, 0 };
+Pixel Image::get(int i) const {
+  return _pixels[i];
 }
 
-void Image::set(int i, const Pixel& c)
-{
+void Image::set(int i, const Pixel& c) {
+  _pixels[i] = c;
 }
 
 Image Image::resize(int w, int h) const {
-   Image result(w, h);
-   return result;
+  Image result(w, h);
+  for (int i = 0; i < h; i++) {
+    float row_ratio = (float) i / (h - 1);
+    int orig_row = floor(row_ratio * (_height - 1));
+    for (int j = 0; j < w; j++) {
+      float col_ratio = (float) j / (w - 1);
+      int orig_col = floor(col_ratio * (_width - 1));
+      result.set(i, j, get(orig_row, orig_col));
+    }
+  }
+  return result;
 }
 
 Image Image::flipHorizontal() const {
-   Image result(0, 0);
-   return result;
-
+  Image result(_width, _height);
+  for (int i = 0; i < _height; i++) {
+    for (int j = 0; j < _width; j++) {
+      result.set(i, j, get(_height - 1 - i, j));
+    }
+  }
+  return result;
 }
 
 Image Image::flipVertical() const {
-   Image result(0, 0);
-   return result;
+  Image result(_width, _height);
+  for (int i = 0; i < _height; i++) {
+    for (int j = 0; j < _width; j++) {
+      result.set(i, j, get(i, _width - 1 - j));
+    }
+  }
+  return result;
 }
 
 Image Image::rotate90() const {
-   Image result(0, 0);
-  
-   return result;
+  Image result(_height, _width);
+  // width and height are switched (b/c image is transposed)
+  for (int i = 0; i < _width; i++) {
+    for (int j = 0; j < _height; j++) {
+      result.set(i, j, get(j, _width - 1 - i));
+    }
+  }
+  return result;
 }
 
 Image Image::subimage(int startx, int starty, int w, int h) const {
-  
-   Image sub(0, 0);
-    return sub;
+  Image sub(w, h);
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      sub.set(i, j, get(starty + i, startx + j));
+    }
+  }
+  return sub;
 }
 
 void Image::replace(const Image& image, int startx, int starty) {
-  
-}
-
-Image Image::swirl() const {
-   Image result(0, 0);
-   return result;
-}
-
-Image Image::add(const Image& other) const {
-   Image result(0, 0);
-  
-   return result;
-}
-
-Image Image::subtract(const Image& other) const {
-   Image result(0, 0);
-   
-   return result;
-}
-
-Image Image::multiply(const Image& other) const {
-   Image result(0, 0);
-   
-   return result;
-}
-
-Image Image::difference(const Image& other) const {
-   Image result(0, 0);
-  
-   return result;
-}
-
-Image Image::lightest(const Image& other) const {
-   Image result(0, 0);
-  
-   return result;
-}
-
-Image Image::darkest(const Image& other) const {
-   Image result(0, 0);
-  
-   return result;
+  // only replace as many pixels as will fit onto original image
+  for (int i = 0; i < _height - starty; i++) {
+    for (int j = 0; j < _width - startx; j++) {
+      set(starty + i, startx + j, image.get(i, j));
+    }
+  }
 }
 
 Image Image::gammaCorrect(float gamma) const {
-
-   Image result(0, 0);
- 
-   return result;
+  Image result(_width, _height);
+  for (int i = 0; i < _height; i++) {
+    for (int j = 0; j < _width; j++) {
+      struct Pixel pixel = get(i,j);  // copy original pixel
+      pixel.r = round(pow((pixel.r / 255.0f), 1.0f / gamma) * 255.0f);
+      pixel.g = round(pow((pixel.g / 255.0f), 1.0f / gamma) * 255.0f);
+      pixel.b = round(pow((pixel.b / 255.0f), 1.0f / gamma) * 255.0f);
+      result.set(i, j, pixel);
+    }
+  }
+  return result;
 }
 
 Image Image::alphaBlend(const Image& other, float alpha) const {
-   Image result(0, 0);
-
-   return result;
-}
-
-Image Image::invert() const {
-   Image image(0, 0);
-   
-   return image;
+  Image result(_width, _height);
+  for (int i = 0; i < _height; i++) {
+    for (int j = 0; j < _width; j++) {
+      struct Pixel under = get(i,j);  // background pixel from this image
+      struct Pixel over = other.get(i, j);  // foreground pixel from other image
+      unsigned char new_red = round((over.r * alpha) + (under.r * (1 - alpha)));
+      unsigned char new_green = round((over.g * alpha) + (under.g * (1 - alpha)));
+      unsigned char new_blue = round((over.b * alpha) + (under.b * (1 - alpha)));
+      struct Pixel corrected = {new_red, new_green, new_blue};
+      result.set(i, j, corrected);
+    }
+  }
+  return result;
 }
 
 Image Image::grayscale() const {
-   Image result(0, 0);
-   
-   return result;
+  Image result(_width, _height);
+  for (int i = 0; i < _height; i++) {
+    for (int j = 0; j < _width; j++) {
+      struct Pixel orig = get(i,j);
+      unsigned char intensity = round((orig.r * 0.3) + (orig.g * 0.59) +
+          (orig.b * 0.11));  // weighted average
+      struct Pixel corrected = {intensity, intensity, intensity};
+      result.set(i, j, corrected);
+    }
+  }
+  return result;
+}
+
+Image Image::add(const Image& other) const {
+  Image result(0, 0);
+
+  return result;
+}
+
+Image Image::subtract(const Image& other) const {
+  Image result(0, 0);
+
+  return result;
+}
+
+Image Image::multiply(const Image& other) const {
+  Image result(0, 0);
+
+  return result;
+}
+
+Image Image::difference(const Image& other) const {
+  Image result(0, 0);
+
+  return result;
+}
+
+Image Image::swirl() const {
+  Image result(0, 0);
+  return result;
+}
+
+Image Image::lightest(const Image& other) const {
+  Image result(0, 0);
+
+  return result;
+}
+
+Image Image::darkest(const Image& other) const {
+  Image result(0, 0);
+
+  return result;
+}
+
+Image Image::invert() const {
+  Image image(0, 0);
+
+  return image;
 }
 
 Image Image::colorJitter(int size) const {
-   Image image(0, 0);
-  
-   return image;
+  Image image(0, 0);
+
+  return image;
 }
 
 Image Image::bitmap(int size) const {
-   Image image(0, 0);
-   
-   return image;
+  Image image(0, 0);
+
+  return image;
 }
 
 void Image::fill(const Pixel& c) {
-  }
+
+}
+
 
 }  // namespace agl
-
--- src/image.h
+++ src/image.h
@@ -10,7 +10,7 @@ namespace agl {
 
 /**
  * @brief Holder for a RGB color
- * 
+ *
  */
 struct Pixel {
     unsigned char r;
@@ -24,27 +24,27 @@ struct Pixel {
 class Image {
  public:
   Image();
-  Image(int width, int height);
+  Image(int width, int height);  // mallocs _pixels based on width and height
   Image(const Image& orig);
   Image& operator=(const Image& orig);
 
   virtual ~Image();
 
-  /** 
-   * @brief Load the given filename 
+  /**
+   * @brief Load the given filename
    * @param filename The file to load, relative to the running directory
-   * @param flip Whether the file should flipped vertally when loaded
-   * 
+   * @param flip Whether the file should flipped vertically when loaded
+   *
    * @verbinclude sprites.cpp
    */
   bool load(const std::string& filename, bool flip = false);
 
-  /** 
+  /**
    * @brief Save the image to the given filename (.png)
    * @param filename The file to load, relative to the running directory
-   * @param flip Whether the file should flipped vertally before being saved
+   * @param flip Whether the file should flipped vertically before being saved
    */
-  bool save(const std::string& filename, bool flip = true) const;
+  bool save(const std::string& filename, bool flip = false) const;
 
   /** @brief Return the image width in pixels
    */
@@ -54,10 +54,10 @@ class Image {
    */
   int height() const;
 
-  /** 
+  /**
    * @brief Return the RGB data
    *
-   * Data will have size width * height * 4 (RGB)
+   * Data will have size width * height * 3 (RGB)
    */
   char* data() const;
 
@@ -66,45 +66,44 @@ class Image {
    * @param width The new image width
    * @param height The new image height
    *
-   * This call will replace the old data with the new data. Data should 
+   * This call will replace the old data with the new data. Data should
    * match the size width * height * 3
    */
   void set(int width, int height, unsigned char* data);
 
   /**
    * @brief Get the pixel at index (row, col)
-   * @param row The row (value between 0 and height)
-   * @param col The col (value between 0 and width)
+   * @param row The row (value between 0 and height-1)
+   * @param col The col (value between 0 and width-1)
    *
    * Pixel colors are unsigned char, e.g. in range 0 to 255
-   */ 
+   */
   Pixel get(int row, int col) const;
 
   /**
-   * @brief Set the pixel RGBA color at index (row, col)
-   * @param row The row (value between 0 and height)
-   * @param col The col (value between 0 and width)
+   * @brief Set the pixel RGB color at index (row, col)
+   * @param row The row (value between 0 and height-1)
+   * @param col The col (value between 0 and width-1)
    *
    * Pixel colors are unsigned char, e.g. in range 0 to 255
-   */ 
+   */
   void set(int row, int col, const Pixel& color);
 
   /**
- * @brief Set the pixel RGB color at index i
- * @param i The index (value between 0 and width * height)
- *
- * Pixel colors are unsigned char, e.g. in range 0 to 255
- */
+  * @brief Set the pixel RGB color at index i
+  * @param i The index (value between 0 and (width * height) - 1)
+  *
+  * Pixel colors are unsigned char, e.g. in range 0 to 255
+  */
   Pixel get(int i) const;
 
   /**
- * @brief Set the pixel RGB color at index i
- * @param i The index (value between 0 and width * height)
- *
- * Pixel colors are unsigned char, e.g. in range 0 to 255
- */
+  * @brief Set the pixel RGB color at index i
+  * @param i The index (value between 0 and (width * height) - 1)
+  *
+  * Pixel colors are unsigned char, e.g. in range 0 to 255
+  */
   void set(int i, const Pixel& c);
-
 
   // resize the image
   Image resize(int width, int height) const;
@@ -115,69 +114,69 @@ class Image {
   // flip around the vertical midline
   Image flipVertical() const;
 
-  // rotate the Image 90 degrees
+  // rotate the Image 90 degrees counter-clockwise
   Image rotate90() const;
 
-  // Return a sub-Image having the given top,left coordinate and (width, height)
+  // Return a sub-Image having the given top left coordinate and (width, height)
   Image subimage(int x, int y, int w, int h) const;
 
   // Replace the portion starting at (row, col) with the given image
   // Clamps the image if it doesn't fit on this image
   void replace(const Image& image, int x, int y);
 
-  // swirl the colors 
-  Image swirl() const;
+  // Apply gamma correction
+  Image gammaCorrect(float gamma) const;
 
-  // Apply the following calculation to the pixels in 
+  // Apply the following calculation to the pixels in
+  // our image and the given image:
+  //    this.pixels = this.pixels * (1-alpha) + other.pixel * alpha
+  // Assumes that the two images are the same size
+  Image alphaBlend(const Image& other, float alpha) const;
+
+  // Convert the image to grayscale
+  Image grayscale() const;
+
+  // Apply the following calculation to the pixels in
   // our image and the given image:
   //    result.pixel = this.pixel + other.pixel
   // Assumes that the two images are the same size
   Image add(const Image& other) const;
 
-  // Apply the following calculation to the pixels in 
+  // Apply the following calculation to the pixels in
   // our image and the given image:
   //    result.pixel = this.pixel - other.pixel
   // Assumes that the two images are the same size
   Image subtract(const Image& other) const;
 
-  // Apply the following calculation to the pixels in 
+  // Apply the following calculation to the pixels in
   // our image and the given image:
   //    result.pixel = this.pixel * other.pixel
   // Assumes that the two images are the same size
   Image multiply(const Image& other) const;
 
-  // Apply the following calculation to the pixels in 
+  // Apply the following calculation to the pixels in
   // our image and the given image:
   //    result.pixel = abs(this.pixel - other.pixel)
   // Assumes that the two images are the same size
   Image difference(const Image& other) const;
 
-  // Apply the following calculation to the pixels in 
+  // swirl the colors
+  Image swirl() const;
+
+  // Apply the following calculation to the pixels in
   // our image and the given image:
   //    result.pixel = max(this.pixel, other.pixel)
   // Assumes that the two images are the same size
   Image lightest(const Image& other) const;
 
-  // Apply the following calculation to the pixels in 
+  // Apply the following calculation to the pixels in
   // our image and the given image:
   //    result.pixel = min(this.pixel, other.pixel)
   // Assumes that the two images are the same size
   Image darkest(const Image& other) const;
 
-  // Apply gamma correction
-  Image gammaCorrect(float gamma) const;
-
-  // Apply the following calculation to the pixels in 
-  // our image and the given image:
-  //    this.pixels = this.pixels * (1-alpha) + other.pixel * alpha
-  // Assumes that the two images are the same size
-  Image alphaBlend(const Image& other, float amount) const;
-
   // Convert the image to grayscale
   Image invert() const;
-
-  // Convert the image to grayscale
-  Image grayscale() const;
 
   // return a bitmap version of this image
   Image colorJitter(int size) const;
@@ -189,7 +188,13 @@ class Image {
   void fill(const Pixel& c);
 
  private:
-   // todo
+  int _width = 0;  // number of columns (in pixels)
+  int _height = 0;  // number of rows (in pixels)
+  int _components = 0; // number of components in original image file
+  // internally use struct Pixel *, externally accept/return as unsigned char *
+  struct Pixel * _pixels;  // internal representation of pixel data
+  bool _need_to_free_stbi = false;  // if true, destructor must stbi free
+  bool _need_to_free_malloc = false;  // if true, destructor must free
 };
 }  // namespace agl
 #endif  // AGL_IMAGE_H_
